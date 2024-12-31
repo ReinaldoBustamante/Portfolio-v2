ce9639a54a7aedb520e98c269ddb1e6060a4cc57
Reinaldo Bustamante <reinaldo.bustamante026@gmail.com>
2024-12-31 14:35:39 -0300

    style: Change gap on text

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,10 @@ export default function HomePage() {
         <h1 className="text-7xl font-bold">Hola, soy Reinaldo</h1>
         <span className="text-3xl font-bold text-[#3384F6] dark:text-[#00ADB5]">Desarrollador full-stack</span>
       </div>
-      <p className="text-[18px]">Ingeniero Civil Informático con experiencia en el desarrollo web. Me apasiona resolver problemas a través de soluciones digitales, siempre buscando la forma más eficiente de optimizar cada proceso.</p>
-      <p className="text-[18px]">Actualmente, he tenido la oportunidad de trabajar en una <span className="text-[#3384F6] dark:text-[#00ADB5] font-bold">plataforma de tutoría inteligente</span> y en un <span className="text-[#3384F6] dark:text-[#00ADB5] font-bold">sistema de diagnóstico temprano de enfermedades</span> para salmones cultivados.</p>
+      <div className="flex flex-col gap-2">
+        <p className="text-[18px]">Ingeniero Civil Informático con experiencia en el desarrollo web. Me apasiona resolver problemas a través de soluciones digitales, siempre buscando la forma más eficiente de optimizar cada proceso.</p>
+        <p className="text-[18px]">Actualmente, he tenido la oportunidad de trabajar en una <Link href={'/experience'} className="text-[#3384F6] dark:text-[#00ADB5] font-bold">plataforma de tutoría inteligente</Link> y en un <Link href={'/experience'} className="text-[#3384F6] dark:text-[#00ADB5] font-bold">sistema de diagnóstico temprano de enfermedades</Link> para salmones cultivados.</p>
+      </div>
       <Link
         href={'/projects'}
         className="text-[20px] py-3 px-4 bg-[#3384F6] text-white dark:bg-[#00ADB5] rounded w-[250px] text-center"
