3f81dec1438de0941ed6a8aa9a14f793e6eabcf2
Reinaldo Bustamante <reinaldo.bustamante026@gmail.com>
2025-01-02 23:07:01 -0300

    chore: delete date

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -13,31 +13,13 @@ export default async function ExperiencePage() {
         <div className="flex mt-4 flex-col gap-8 w-full">
           {
             experiences.map((experience) => {
-
-              const startDate = new Date(experience.startDate);
-              const endDate = new Date(experience.endDate);
-
-              // Utilizamos toLocaleDateString con la zona horaria de Chile
-              const formattedStartDate = startDate.toLocaleDateString('es-ES', {
-                timeZone: 'America/Santiago',
-                month: 'long',
-                year: 'numeric'
-              }).replace(/^\w/, (c) => c.toUpperCase());
-              const formattedEndDate = endDate.toLocaleDateString('es-ES', {
-                timeZone: 'America/Santiago',
-                month: 'long',
-                year: 'numeric'
-              }).replace(/^\w/, (c) => c.toUpperCase());
-
-              const formattedRange = `${formattedStartDate} - ${formattedEndDate}`;
-
               return <div className="flex items-start w-full" key={experience.id}>
                 <span className="bg-[#3384F6] dark:bg-[#00ADB5] w-5 h-5 rounded-full absolute left-0 mt-1" />
                 <div className="ml-4 w-full flex flex-col gap-3">
                   <div className="flex flex-col">
                     <h2 className="text-2xl font-semibold">{experience.position}</h2>
                     <span className="font-semibold text-blue-500 dark:text-[#00ADB5] text-xl">{experience.company}</span>
-                    <span className="text-gray-500 text-lg">{formattedRange} </span>
+
 
                   </div>
                   <p className="w-full text-[18px]">{experience.description}</p>
